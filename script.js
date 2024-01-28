c16ab306ed3beae8754443d52a7f377035ee804c
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-29 01:36:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let arr = [1, -2, 3, 0, 4, -5, 6, -11];
+
+let positiveNumbers, result;
+
+positiveNumbers = arr.filter(num => num > 0);
+result = positiveNumbers.map(num => Math.sqrt(num));
+
+console.log('Method filter and map:', result);
+
+result = [];
+
+arr.forEach(num => {
+	if (num > 0) {
+		result.push(Math.sqrt(num));
+	}
+});
+
+console.log('Method forEach:', result);
+
+result = [];
+positiveNumbers = arr.filter(num => num > 0);
+
+positiveNumbers.forEach(num => {
+	result.push(Math.sqrt(num));
+});
+
+console.log('Method filter and forEach:', result);
+
+result = arr.map(number => {
+	return number > 0 ? Math.sqrt(number) : null;
+});
+
+result = result.filter(result => result !== null);
+
+console.log('Method map and forEach:', result);
