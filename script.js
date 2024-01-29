bad907f5c3a7f52068f30e05b0b14736c1fdf1c2
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-29 13:11:23 +0200

    Updated js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let arr = [1, -2, 3, 0, 4, -5, 6, -11];
-
 let positiveNumbers, result;
 
+/* Method filter and map */
 positiveNumbers = arr.filter(num => num > 0);
 result = positiveNumbers.map(num => Math.sqrt(num));
 
 console.log('Method filter and map:', result);
 
+/* Method forEach */
 result = [];
 
 arr.forEach(num => {
@@ -17,15 +18,17 @@ arr.forEach(num => {
 
 console.log('Method forEach:', result);
 
+/* Method filter and forEach */
 result = [];
-positiveNumbers = arr.filter(num => num > 0);
 
+positiveNumbers = arr.filter(num => num > 0);
 positiveNumbers.forEach(num => {
 	result.push(Math.sqrt(num));
 });
 
 console.log('Method filter and forEach:', result);
 
+/* Method map and forEach */
 result = arr.map(number => {
 	return number > 0 ? Math.sqrt(number) : null;
 });
